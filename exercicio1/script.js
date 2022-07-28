a17c1425fe08bfa65e6c15549e46793988e4dfa4
rogerio kayronny <kayronnysales@gmail.com>
2022-07-28 19:47:58 -0300

    incluindo tarefa exercicio 1

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,7 +1,7 @@
 //  O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
 // - O segundo array deve possuir apenas **strings**;
 // - O terceiro array deve possuir **números**, **strings** e **booleanos**.
-// Imprima cada um dos arrays acima, utilizando `console.log()` *// 
+// Imprima cada um dos arrys acima, utilizando `console.log()` *// 
 
 const numero = [3,1,9,6];
 const comidas = ["burguer lasanha cuzcuz"];
